cf97b6bfefbc2832d161a2c0fde6d6cdf180761d
Nicole Glasgow <nicole.glasgow@ymail.com>
2013-07-26 13:02:15 -0700

    Create the auction app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReverseAuction::Application.config.secret_token = '6562365775f597ebedadfe12cacf1e7758d16c17b4529d19ee323a8383270ec22c6821567ec3d82e4dcdaa112196581ed0871e45437c870fbf4be3461cb78249'
