0fb79d67f663c4761db54727191c7a720ae2435d
Nicole Glasgow <nicole.glasgow@ymail.com>
2013-07-30 16:19:00 -0700

    correct patient sign up error

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,7 +3,7 @@ class Patient < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
   attr_accessible :email, :password, :password_confirmation
 end
--- spec/integration/patient_signing_in_spec.rb
+++ spec/integration/patient_signing_in_spec.rb
@@ -6,7 +6,7 @@ feature 'Patient Signing in' do
   end
 
   scenario 'Patient Signing in via confirmation' do
-    open_email "patient@example.com", :with_subject => /Confirmation/
+    open_email "patient@example.com", with_subject: /Confirmation/
     click_first_link_in_email
     page.should have_content("Your account was successfully confirmed")
     page.should have_content("Signed in as patient@example.com")
