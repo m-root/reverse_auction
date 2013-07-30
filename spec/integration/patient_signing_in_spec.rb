8adcfe7be4bfa0ce5513af847225525771971301
Nicole Glasgow <nicole.glasgow@ymail.com>
2013-07-30 16:40:53 -0700

    add signing in feature

--- spec/integration/patient_signing_in_spec.rb
+++ spec/integration/patient_signing_in_spec.rb
@@ -11,4 +11,14 @@ feature 'Patient Signing in' do
     page.should have_content("Your account was successfully confirmed")
     page.should have_content("Signed in as patient@example.com")
   end
+
+  scenario 'Patient Signing in via form' do
+    Patient.find_by_email('patient@example.com').confirm!
+    visit '/'
+    click_link 'Patient Sign in'
+    fill_in 'Email', with: "patient@example.com"
+    fill_in 'Password', with: "password"
+    click_button "Sign in"
+    page.should have_content("Signed in successfully.")
+  end
 end
